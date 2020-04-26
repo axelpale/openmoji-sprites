3d141e52ae359966983c4b8a0db9052a14381d5a
Akseli Palen <akseli.palen@gmail.com>
2020-04-26 19:11:52 +0300

    Preliminary config file to drive the sheet and index generation

---
+++ config.js
@@ -0,0 +1,209 @@
+/* eslint-disable quote-props */
+
+module.exports = {
+  includeGroups: [
+    'smileys-emotion',
+    'people-body',
+    'animals-nature',
+    'food-drink',
+    'travel-places',
+    'activities',
+    'objects',
+    'symbols',
+    'extras-openmoji'
+  ],
+  groups: {
+    'smileys-emotion': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'face-smiling',
+            'face-affection',
+            'face-tongue',
+            'face-hand',
+            'face-neutral-skeptical',
+            'face-sleepy',
+            'face-unwell',
+            'face-hat',
+            'face-glasses',
+            'face-concerned',
+            'face-negative',
+            'face-costume',
+            'cat-face',
+            'monkey-face',
+            'emotion'
+          ]
+        }
+      ]
+    },
+    'people-body': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'hand-fingers-open',
+            'hand-fingers-partial',
+            'hand-single-finger',
+            'hand-fingers-closed',
+            'hands',
+            'hand-prop',
+            'body-parts',
+            'person',
+            'person-gesture',
+            'person-role',
+            'person-fantasy',
+            'person-activity',
+            'person-sport',
+            'person-resting',
+            'family',
+            'person-symbol'
+          ]
+        }
+      ]
+    },
+    'animals-nature': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'animal-mammal',
+            'animal-bird',
+            'animal-amphibian',
+            'animal-reptile',
+            'animal-marine',
+            'animal-bug',
+            'plant-flower',
+            'plant-other'
+          ]
+        }
+      ]
+    },
+    'food-drink': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'food-fruit',
+            'food-vegetable',
+            'food-prepared',
+            'food-asian',
+            'food-marine',
+            'food-sweet',
+            'drink',
+            'dishware'
+          ]
+        }
+      ]
+    },
+    'travel-places': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'place-map',
+            'place-geographic',
+            'place-building',
+            'place-religious',
+            'place-other',
+            'transport-ground',
+            'transport-water',
+            'transport-air',
+            'hotel',
+            'time',
+            'sky-weather'
+          ]
+        }
+      ]
+    },
+    'activities': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'event',
+            'award-medal',
+            'sport',
+            'game',
+            'arts-crafts'
+          ]
+        }
+      ]
+    },
+    'objects': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'clothing',
+            'sound',
+            'music',
+            'musical-instrument',
+            'phone',
+            'computer',
+            'light-video',
+            'book-paper',
+            'money',
+            'mail',
+            'writing',
+            'office',
+            'lock',
+            'tool',
+            'science',
+            'medical',
+            'household',
+            'other-object'
+          ]
+        }
+      ]
+    },
+    'symbols': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'transport-sign',
+            'warning',
+            'arrow',
+            'religion',
+            'zodiac',
+            'av-symbol',
+            'gender',
+            'math',
+            'punctuation',
+            'currency',
+            'other-symbol',
+            'keycap',
+            'alphanum',
+            'geometric'
+          ]
+        }
+      ]
+    },
+    'flags': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'flag',
+            'country-flag',
+            'subdivision-flag'
+          ]
+        }
+      ]
+    },
+    'extras-openmoji': {
+      sheets: [
+        {
+          includeSubgroups: [
+            'animals-nature',
+            'brand',
+            'climate-environment',
+            'emergency',
+            'food-drink',
+            'healthcare',
+            'interaction',
+            'objects',
+            'people',
+            'smileys-emotion',
+            'symbols',
+            'technology',
+            'travel-places',
+            'ui-element'
+          ]
+        }
+      ]
+    }
+  }
+}
